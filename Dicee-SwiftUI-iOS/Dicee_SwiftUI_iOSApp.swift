f64ecd62216830aab45998db97d50dd406b003d3
NijatNaghiyev <nicat614@icloud.com>
2025-01-26 18:22:35 +0400

    Initial Commit

---
+++ Dicee-SwiftUI-iOS/Dicee_SwiftUI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_iOSApp.swift
+//  Dicee-SwiftUI-iOS
+//
+//  Created by Nijat Naghiyev on 26.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
